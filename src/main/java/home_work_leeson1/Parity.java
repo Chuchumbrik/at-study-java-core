c6486cebcb49feacb6c3ab27b28d7c866041eb05
Chuchumbrik <mussha2010@yandex.ru>
2021-04-16 20:17:27 +0300

    Выполнено задание #3 (lesson #1)

---
+++ src/main/java/home_work_leeson1/Parity.java
@@ -0,0 +1,14 @@
+package home_work_leeson1;
+
+public class Parity {
+
+    boolean checkParityNumber(int number){
+        return number%2 == 1;
+    }
+
+    public static void main(String[] args) {
+        Parity parity = new Parity();
+        System.out.println(parity.checkParityNumber(14));
+        System.out.println(parity.checkParityNumber(19));
+    }
+}
