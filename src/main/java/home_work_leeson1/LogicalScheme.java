d35e3899d15ad72fe0db4fc51e03fd3d8c8ff574
Chuchumbrik <mussha2010@yandex.ru>
2021-04-17 12:15:58 +0300

    Исправил название матода Задание #6 (lesson #1)

--- src/main/java/home_work_leeson1/LogicalScheme.java
+++ src/main/java/home_work_leeson1/LogicalScheme.java
@@ -2,16 +2,16 @@ package home_work_leeson1;
 
 public class LogicalScheme {
 
-    boolean logicalResult(boolean a, boolean b, boolean c, boolean d){
+    boolean getSchemeResult(boolean a, boolean b, boolean c, boolean d){
         return (a && b) && !(c || d);
     }
 
     public static void main(String[] args) {
         LogicalScheme logicalScheme = new LogicalScheme();
-        System.out.println(logicalScheme.logicalResult(false, false, false, false));
-        System.out.println(logicalScheme.logicalResult(true, true, true, true));
-        System.out.println(logicalScheme.logicalResult(false, true, false, true));
-        System.out.println(logicalScheme.logicalResult(true, false, true, false));
-        System.out.println(logicalScheme.logicalResult(true, true, false, false));
+        System.out.println(logicalScheme.getSchemeResult(false, false, false, false));
+        System.out.println(logicalScheme.getSchemeResult(true, true, true, true));
+        System.out.println(logicalScheme.getSchemeResult(false, true, false, true));
+        System.out.println(logicalScheme.getSchemeResult(true, false, true, false));
+        System.out.println(logicalScheme.getSchemeResult(true, true, false, false));
     }
 }
