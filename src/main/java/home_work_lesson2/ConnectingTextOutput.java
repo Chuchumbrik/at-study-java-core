33cef0c4a00235d394d0efe68d83a05c72b952f2
Chuchumbrik <mussha2010@yandex.ru>
2021-04-17 22:30:39 +0300

    Выполнено задание #1 (lesson #2)

---
+++ src/main/java/home_work_lesson2/ConnectingTextOutput.java
@@ -0,0 +1,41 @@
+package home_work_lesson2;
+
+public class ConnectingTextOutput {
+
+    void printFor(String[] stringArray) {
+        String result = "";
+        for (int i = 0; i < stringArray.length; i++){
+            result += stringArray[i];
+            result += (i == stringArray.length - 1) ? "." : " ";
+        }
+        System.out.println(result);
+    }
+
+    void printForeach(String[] stringArray) {
+        String result = "";
+        for (String string: stringArray)
+            result += string + " ";
+        result = result.trim() + ".";
+        System.out.println(result);
+    }
+
+    void printWhile(String[] stringArray) {
+        String result = "";
+        int i = 0;
+        while (i < stringArray.length) {
+            result += stringArray[i];
+            result += (i == stringArray.length - 1) ? "." : " ";
+            i++;
+        }
+        System.out.println(result);
+    }
+
+
+    public static void main(String[] args) {
+        ConnectingTextOutput ConnectingTextOutput = new ConnectingTextOutput();
+        String[] stringArray = {"Пятница", "это", "лучший", "день", "недели"};
+        ConnectingTextOutput.printFor(stringArray);
+        ConnectingTextOutput.printForeach(stringArray);
+        ConnectingTextOutput.printWhile(stringArray);
+    }
+}
