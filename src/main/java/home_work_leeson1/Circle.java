574a5860bc731a673f7c47c63a545678fe566c16
Chuchumbrik <mussha2010@yandex.ru>
2021-04-17 12:05:02 +0300

    Исправил название метода Задание #1 (lesson #1)

--- src/main/java/home_work_leeson1/Circle.java
+++ src/main/java/home_work_leeson1/Circle.java
@@ -4,14 +4,14 @@ package home_work_leeson1;
 public class Circle {
     final double PI = 3.14;
 
-    double square(int radius) {
+    double getSquare(int radius) {
         return radius*radius*PI;
     }
 
     public static void main(String[] args) {
         Circle circle = new Circle();
-        System.out.println(circle.square(0));
-        System.out.println(circle.square(2));
-        System.out.println(circle.square(100));
+        System.out.println(circle.getSquare(0));
+        System.out.println(circle.getSquare(2));
+        System.out.println(circle.getSquare(100));
     }
 }
