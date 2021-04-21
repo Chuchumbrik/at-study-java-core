823e209c700278199eba13b978e294ee7ec098f1
Chuchumbrik <mussha2010@yandex.ru>
2021-04-21 23:14:43 +0300

    Выполнено задание #8 (lesson #2)

---
+++ src/main/java/lesson2/task8/PrintA.java
@@ -0,0 +1,58 @@
+package lesson2.task8;
+
+public class PrintA {
+    public static void main(String[] args) {
+        PrintA printA = new PrintA();
+        int[] numbers = {-1, 2, 4, 7, 0};
+        printA.outputQuantityHandlerOptionOne(numbers);
+        printA.outputQuantityHandlerOptionTwo(numbers);
+    }
+
+    void outputQuantityHandlerOptionOne(int... numbers) {
+        for (int number: numbers) {
+            String result = "";
+
+            switch (number) {
+                case 1:
+                case 2:
+                case 3:
+                case 4:
+                    for (int i = 0; i < number; i++){
+                        result += "A";
+                    }
+                    break;
+                default:
+                    result = "AAAAA";
+            }
+            System.out.println(result);
+        }
+        System.out.println();
+    }
+
+    void outputQuantityHandlerOptionTwo(int... numbers) {
+
+        for (int number: numbers) {
+            boolean outputFlag = false;
+            String result = "";
+
+            switch (number) {
+                case 1:
+                case 2:
+                case 3:
+                case 4:
+                    for (int i = 0; i < number; i++){
+                        result += "A";
+                    }
+                    outputFlag = true;
+                default:
+                    if (!outputFlag) {
+                        result = "AAAAA";
+                    }
+            }
+            System.out.println(result);
+        }
+        System.out.println();
+    }
+
+
+}
