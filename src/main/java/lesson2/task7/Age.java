6ecccb82b52d22a12560ed7b830e1d18b1d61158
Chuchumbrik <mussha2010@yandex.ru>
2021-04-21 23:00:55 +0300

    Выполнено задание #7 (lesson #2)

---
+++ src/main/java/lesson2/task7/Age.java
@@ -0,0 +1,29 @@
+package lesson2.task7;
+
+public class Age {
+    public static void main(String[] args) {
+        Age age = new Age();
+        int[] ageArray = {32, 52, 66, 82, 25, 44, 60, 75, 22, 119};
+        age.textOutputAge(ageArray);
+    }
+
+    void textOutputAge(int... ages) {
+        String result;
+        for (int age: ages) {
+            if ( age >= 25 && age < 44) {
+                result = "Молодой";
+            } else
+            if ( age >= 44 && age < 60) {
+                result = "Средний";
+            } else
+            if ( age >= 60 && age < 75) {
+                result = "Пожилой";
+            } else
+            if ( age >= 75 && age < 90) {
+                result = "Старческий";
+            } else
+                result = "Неизвестный";
+            System.out.println(result + " возраст");
+        }
+    }
+}
