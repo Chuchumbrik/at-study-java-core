bf3ea6a9e0df94264291e706c521fb72cc4b75bc
Chuchumbrik <mussha2010@yandex.ru>
2021-04-22 13:32:25 +0300

    Разобрал решение без break в ответе Задание #8 (lesson #2)

--- src/main/java/lesson2/task8/PrintA.java
+++ src/main/java/lesson2/task8/PrintA.java
@@ -31,25 +31,41 @@ public class PrintA {
 
     void outputQuantityHandlerOptionTwo(int... numbers) {
 
-        for (int number: numbers) {
-            boolean outputFlag = false;
-            String result = "";
+//        for (int number: numbers) {
+//            boolean outputFlag = false;
+//            String result = "";
+//
+//            switch (number) {
+//                case 1:
+//                case 2:
+//                case 3:
+//                case 4:
+//                    for (int i = 0; i < number; i++){
+//                        result += "A";
+//                    }
+//                    outputFlag = true;
+//                default:
+//                    if (!outputFlag) {
+//                        result = "AAAAA";
+//                    }
+//            }
+//            System.out.println(result);
+//        }
+//        System.out.println();
 
+        for (int number: numbers) {
             switch (number) {
-                case 1:
-                case 2:
-                case 3:
-                case 4:
-                    for (int i = 0; i < number; i++){
-                        result += "A";
-                    }
-                    outputFlag = true;
                 default:
-                    if (!outputFlag) {
-                        result = "AAAAA";
-                    }
+                    System.out.print("A");
+                case 4:
+                    System.out.print("A");
+                case 3:
+                    System.out.print("A");
+                case 2:
+                    System.out.print("A");
+                case 1:
+                    System.out.print("A");
             }
-            System.out.println(result);
         }
         System.out.println();
     }
