d299ba7c0b6b1527f40974f2b91fdf5700f5c0c1
Chuchumbrik <mussha2010@yandex.ru>
2021-04-22 13:11:12 +0300

    Разобрал решение в ответе Задание #4 (lesson #2)

--- src/main/java/lesson2/task4/IdenticalCharacters.java
+++ src/main/java/lesson2/task4/IdenticalCharacters.java
@@ -6,25 +6,42 @@ public class IdenticalCharacters {
 
     public static void main(String[] args) {
         IdenticalCharacters identicalCharacters = new IdenticalCharacters();
-        System.out.println(identicalCharacters.makingStringOfIdenticalCharacters("Hello","World"));
-        System.out.println(identicalCharacters.makingStringOfIdenticalCharacters("ping","pong"));
-        System.out.println(identicalCharacters.makingStringOfIdenticalCharacters("pong","asdf"));
-        System.out.println(identicalCharacters.makingStringOfIdenticalCharacters("post","get"));
+        System.out.println(identicalCharacters.makingStringOfIdenticalCharacters1("Hello","World"));
+        System.out.println(identicalCharacters.makingStringOfIdenticalCharacters1("ping","pong"));
+        System.out.println(identicalCharacters.makingStringOfIdenticalCharacters1("pong","asdf"));
+        System.out.println(identicalCharacters.makingStringOfIdenticalCharacters1("post","get"));
     }
 
-    String makingStringOfIdenticalCharacters(String stringFirst, String stringSecond){
-        String result = "", test;
-        for (int i = 0; i < stringFirst.length(); i++) {
-            for (int j = 0; j < stringSecond.length(); j++) {
-                if (stringFirst.charAt(i) == stringSecond.charAt(j)){
-                    test = stringFirst.charAt(i) + "";
-                    if (!result.contains(test))
-                        result += stringFirst.charAt(i);
-                }
-            }
-        }
-        char tempArray[] = result.toCharArray();
+//    String makingStringOfIdenticalCharacters(String stringFirst, String stringSecond){
+//        String result = "", test;
+//        for (int i = 0; i < stringFirst.length(); i++) {
+//            for (int j = 0; j < stringSecond.length(); j++) {
+//                if (stringFirst.charAt(i) == stringSecond.charAt(j)){
+//                    test = stringFirst.charAt(i) + "";
+//                    if (!result.contains(test))
+//                        result += stringFirst.charAt(i);
+//                }
+//            }
+//        }
+//        char tempArray[] = result.toCharArray();
+//        Arrays.sort(tempArray);
+//        return new String(tempArray);
+//    }
+
+
+    //Супер изящно по сравнению с моим решенир
+    String makingStringOfIdenticalCharacters1(String stringFirst, String stringSecond){
+        String result = "";
+
+        char tempArray[] = stringFirst.toCharArray();
         Arrays.sort(tempArray);
-        return new String(tempArray);
+
+        for (char item: tempArray) {
+            String itemChar = String.valueOf(item);
+            if (result.contains(itemChar)) continue;
+            if (stringSecond.contains(itemChar)) result += itemChar;
+        }
+
+        return result;
     }
 }
